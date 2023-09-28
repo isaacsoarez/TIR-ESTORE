33bf2292983dfd6938eb1fc1290e3a376e2ddf78
Isaac Soares <132666435+isaacsoarez@users.noreply.github.com>
2023-09-28 16:35:58 -0300

    Add files via upload

---
+++ Mercedes-Formativa/templates/front-end/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        MateriaPrima: 'Aço',
+        Produção: '85743',
+        Fornecedor: 'Yokohama',
+        FreteKM: '36.3 KM'
+    },
+    {
+        productName: 'CSS Full Course',
+        productNumber: '97245',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Flex-Box Tutorial',
+        productNumber: '36452',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
